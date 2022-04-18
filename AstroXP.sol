d762d38b8f5718e5fdd28e158a9c9b840b4f04d0
Nio Martinez <niosupetranmartinez@gmail.com>
2022-04-19 04:34:38 +0800

    Contract Updates

--- AstroXP.sol
+++ AstroXP.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import "./ERC20/ERC20.sol";
 
-contract ANTON is ERC20 {
-    constructor(uint256 initialSupply) ERC20("ANTON", "NIO") {
+contract AstroXP is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Astro XP", "AXP") {
          _mint(msg.sender, initialSupply * (10 ** uint256(decimals()))); 
     }
 }
